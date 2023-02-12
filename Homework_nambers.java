90d8cd82884d74dddec2139a24eeb5fb809f65a7
shterenberg76 <123587702+shterenberg76@users.noreply.github.com>
2023-02-12 18:24:11 +0100

    HomeWork

---
+++ Homework_nambers.java
@@ -0,0 +1,97 @@
+public class Homework_nambers {
+    public static void main (String[] args){
+
+        int a16 = 333;
+        System.out.println("Result (16->10) ="+ ( 3*Math.pow (16,2) +3*Math.pow(16,1) +3*Math.pow(16,0)));
+
+        int a = 200345;
+        System.out.println(a);
+        System.out.println(2*100000 +3*100 +4*10 +5*1);
+
+        int b = 637;
+        System.out.println(637/16);
+        System.out.println(637%16);
+        System.out.println();
+
+        System.out.println(39/16);
+        System.out.println(39%16);
+        System.out.println();
+
+        System.out.println(2/16);
+        System.out.println(2%16);
+        System.out.println();
+
+        int b16 =02713;
+        System.out.println("Result (16->10) ="+ ( 2*Math.pow(16,2) +7*Math.pow(16,1) +13*Math.pow(16,0)));
+
+        int c = 637;
+        System.out.println(637/2);
+        System.out.println(637%2);
+        System.out.println();
+
+        System.out.println(318/2);
+        System.out.println(318%2);
+        System.out.println();
+
+        System.out.println(159/2);
+        System.out.println(159%2);
+        System.out.println();
+
+        System.out.println(79/2);
+        System.out.println(79%2);
+        System.out.println();
+
+        System.out.println(39/2);
+        System.out.println(39%2);
+        System.out.println();
+
+        System.out.println(19/2);
+        System.out.println(19%2);
+        System.out.println();
+
+        System.out.println(9/2);
+        System.out.println(9%2);
+        System.out.println();
+
+        System.out.println(4/2);
+        System.out.println(4%2);
+        System.out.println();
+
+        System.out.println(2/2);
+        System.out.println(2%2);
+        System.out.println();
+
+        System.out.println("Result (10->2) ="+Integer.toBinaryString(c));
+
+        int d2 = 11100111;
+        System.out.println("Result (2->10) ="+ ( 1*Math.pow (2,7) +1*Math.pow(2,6) +1*Math.pow(2,5) +0*Math.pow (2,4) +0*Math.pow(2,3) +1*Math.pow(2,2)+1*Math.pow(2,1) +1*Math.pow(2,0)));
+
+        int e = 637;
+        System.out.println(637/3);
+        System.out.println(637%3);
+        System.out.println();
+
+        System.out.println(212/3);
+        System.out.println(212%3);
+        System.out.println();
+
+        System.out.println(70/3);
+        System.out.println(70%3);
+        System.out.println();
+
+        System.out.println(23/3);
+        System.out.println(23%3);
+        System.out.println();
+
+        System.out.println(7/3);
+        System.out.println(7%3);
+        System.out.println();
+
+        System.out.println(2/3);
+        System.out.println(2%3);
+        System.out.println();
+
+        
+
+    }
+}
